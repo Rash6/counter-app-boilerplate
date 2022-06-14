c95786b4f9769878250939626ff082ae94074a7d
Rash6 <rashmirohini20@gmail.com>
2022-06-14 19:26:09 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const[counter,setCounter]=useState(0)
+  const handleClickdec=()=>{
+    if(counter===0){
+      return
+    }
+    setCounter(counter-1)
+  }
+  const handleClickinc=()=>{
+    setCounter(counter+1)
+  }
   return (
     <div className="App">
-      <h2 data-testid="counter-value">X</h2>
-      <button data-testid="counter-decrement-button"></button>
-      <button data-testid="counter-increment-button"></button>
+      <h2 data-testid="counter-value">{counter}</h2>
+      <button onClick={handleClickdec} data-testid="counter-decrement-button">-</button>
+      <button onClick={handleClickinc} data-testid="counter-increment-button">+</button>
     </div>
   );
 }
